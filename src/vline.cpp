56f4594f12e776b8acdc02dd4ef7aec0b363f448
BenGarmanBB <137502608+BenGarmanBB@users.noreply.github.com>
2024-04-06 21:41:32 +0100

    Update vline.cpp

--- src/vline.cpp
+++ src/vline.cpp
@@ -11,20 +11,20 @@
 namespace uICAL {
     VLine::VLine(const string& line) {
         if(line.empty()) {
-            // this->name = string::none();
-            // this->value = string::none();
+            this->name = string::none();
+            this->value = string::none();
             log_error("%s", "VLINE is empty");
-            throw ParseError("VLINE is empty");
+            // throw ParseError("VLINE is empty");
         }
 
         size_t colon = line.find(":");
         size_t semicolon = line.find(";");
 
         if (colon == string::npos) {
-            // this->name = string::none();
-            // this->value = string::none();
+            this->name = string::none();
+            this->value = string::none();
             log_error("VLINE does not have a ':' \"%s\"", line.c_str());
-            throw ParseError(string("VLINE does not have a ':' \"") + line + "\"");
+            // throw ParseError(string("VLINE does not have a ':' \"") + line + "\"");
         }
 
         if (semicolon != string::npos && semicolon < colon) {
@@ -51,7 +51,7 @@ namespace uICAL {
             size_t equals = token.find("=");
             if (equals == string::npos) {
                 log_error("Bad param: \"%s\"", token.c_str());
-                throw ParseError(string("Bad param: ") + token);
+                // throw ParseError(string("Bad param: ") + token);
             }
             const string name = token.substr(0, equals);
             const string value = token.substr(equals + 1, token.length());
